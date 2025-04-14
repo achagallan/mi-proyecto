e6c6d982c9d95c02d87e81710c4844087bc3a296
achagallan <ANGEL9811ENGIEL@GMAIL.COM>
2025-04-14 17:48:33 -0500

    Update db.php

--- mi_tienda/db.php
+++ mi_tienda/db.php
@@ -1,13 +1,13 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "botica_guerrero";
+$username = "usuario";  // El nombre de usuario que creaste
+$password = "tu_contraseña";  // La contraseña que asignaste
+$dbname = "miproyecto";  // El nombre de la base de datos que creaste
 
 // Crear conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verificar conexión
+// Verificar la conexión
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
